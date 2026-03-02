5219af47503b8266c8b906a528c1e2cec4ae9717
Nelia <nelia.fedele@imt-atlantique.net>
2026-03-02 11:41:58 +0100

    Nouveau fichier pour PWA + rearrangement du git

---
+++ Fichier racines/index.php
@@ -0,0 +1,38 @@
+<?php
+$jours = ['lundi', 'mardi', 'mercredi', 'jeudi', 'vendredi'];
+$periodes = ['midi', 'soir'];
+?>
+
+<!doctype html>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="theme-color" content="#27ae60">
+    <link rel="manifest" href="manifest.json">
+    <title>Sondage RAK</title>
+</head>
+<body>
+    <div id="rak-survey">
+        <form action="vote.php" method="POST">
+            <?php foreach ($jours as $jour): ?>
+                <?php foreach ($periodes as $periode): ?>
+                    <?php $nomChamp = 'presence_' . $jour . '_' . $periode; ?>
+                    <h3>Mangerez-vous au RAK <?php echo $jour; ?> <?php echo $periode; ?> ?</h3>
+                    <label>
+                        <input type="radio" name="<?php echo $nomChamp; ?>" value="oui" required> Oui, je viens !
+                    </label><br>
+                    <label>
+                        <input type="radio" name="<?php echo $nomChamp; ?>" value="non"> Non, pas cette fois.
+                    </label><br>
+                    <label>
+                        <input type="radio" name="<?php echo $nomChamp; ?>" value="peut-etre"> Je ne sais pas encore.
+                    </label><br><br>
+                <?php endforeach; ?>
+            <?php endforeach; ?>
+
+            <button type="submit">Envoyer ma réponse</button>
+        </form>
+    </div>
+</body>
+</html>
